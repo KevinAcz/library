8b28347df36e8b41d32183f5f861fec506a512ea
KevinAcz <kevinaceitonsalazar@gmail.com>
2024-02-24 19:00:47 -0300

    Modal works now, and now every card has an ID.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 const myLibrary = [];
+//TODO:
+//ADD BUTTON TO DELETE CARD (USE ID TO IDENTIFY IT)
+//
+function Book(id, author, title, pages, read) {
 
-function Book(author, title, pages, read) {
-
+    this.id = id;
     this.author = author;
     this.title = title;
     this.pages = pages;
@@ -10,7 +13,12 @@ function Book(author, title, pages, read) {
 }
 
 const submit = document.querySelector('#submit');
+submit.addEventListener('click', () => {
+    modal.close();
+})
 
+    
+let id = 1;
 function submitForm(event) {
 
     event.preventDefault();
@@ -21,28 +29,16 @@ function submitForm(event) {
     let read = document.querySelector('#read').checked;
 
     
-    let newBook = new Book(author, title, pages, read);
+    let newBook = new Book(id, author, title, pages, read);
     myLibrary.push(newBook);
 
-    submit.addEventListener('click', () => {
-        modal.close();
-    })
+    let container = document.querySelector('#book-list');
 
-}
-
-const openModal = document.querySelector('#open-button');
-
-openModal.addEventListener('click', () => {
-    modal.showModal()    
-});
-
-let container = document.querySelector('#book-list');
-    
-for (const book of myLibrary) {
-    console.log(book.author); //inserta contenido de cada instancia del objeto
     let cardContainer = document.createElement('div');
     cardContainer.classList.add('card-container');
 
+    let cardId = document.createElement('h1');
+    cardContainer.appendChild(cardId);
     let cardTitle = document.createElement('h1');
     cardContainer.appendChild(cardTitle);
 
@@ -57,15 +53,57 @@ for (const book of myLibrary) {
 
     container.appendChild(cardContainer);
 
-    cardTitle.textContent = book.title;
-    cardAuthor.textContent = book.author;
-    cardPages.textContent = book.pages;
-    cardRead.textContent = book.read;
+    cardId.textContent = id;
+    id += 1;
+    cardTitle.textContent = title;
+    cardAuthor.textContent = author;
+    cardPages.textContent = pages;
+    cardRead.textContent = read;
     cardContainer.appendChild(cardTitle);
     cardContainer.appendChild(cardAuthor);
     cardContainer.appendChild(cardPages);
     cardContainer.appendChild(cardRead);
+    console.log(myLibrary)
 }
+
+const openModal = document.querySelector('#open-button');
+
+openModal.addEventListener('click', () => {
+    modal.showModal()    
+});
+
+let container = document.querySelector('#book-list');
+
+
+// LOOP IN EVERY INSTANCE OF THE OBJECT
+// for (const book of myLibrary) {
+//     console.log(book.author); //inserta contenido de cada instancia del objeto
+//     let cardContainer = document.createElement('div');
+//     cardContainer.classList.add('card-container');
+
+//     let cardTitle = document.createElement('h1');
+//     cardContainer.appendChild(cardTitle);
+
+//     let cardAuthor = document.createElement('h2');
+//     cardContainer.appendChild(cardAuthor);
+
+//     let cardPages = document.createElement('p');
+//     cardContainer.appendChild(cardPages);
+
+//     let cardRead = document.createElement('p');
+//     cardContainer.appendChild(cardRead);
+
+//     container.appendChild(cardContainer);
+
+//     cardTitle.textContent = book.title;
+//     cardAuthor.textContent = book.author;
+//     cardPages.textContent = book.pages;
+//     cardRead.textContent = book.read;
+//     cardContainer.appendChild(cardTitle);
+//     cardContainer.appendChild(cardAuthor);
+//     cardContainer.appendChild(cardPages);
+//     cardContainer.appendChild(cardRead);
+// }
 
 //TODO: Associate every book to an id
 // console.log(myLibrary);
