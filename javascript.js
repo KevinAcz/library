d2a1d89f24074eeb5b66713717e24525637f959b
KevinAcz <kevinaceitonsalazar@gmail.com>
2024-02-25 18:03:32 -0300

    showCards added

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@ const myLibrary = [];
 //TODO:
 //ADD BUTTON TO DELETE CARD (USE ID TO IDENTIFY IT)
 //
-function Book(id, author, title, pages, read) {
+function Book( author, title, pages, read) {
 
-    this.id = id;
+    // this.id = id;
     this.author = author;
     this.title = title;
     this.pages = pages;
@@ -18,7 +18,7 @@ submit.addEventListener('click', () => {
 })
 
     
-let id = 1;
+// let id = 1;
 function submitForm(event) {
 
     event.preventDefault();
@@ -28,86 +28,56 @@ function submitForm(event) {
     let pages = document.querySelector('#pages').value;
     let read = document.querySelector('#read').checked;
 
-    
-    let newBook = new Book(id, author, title, pages, read);
+    let newBook = new Book(author, title, pages, read);
     myLibrary.push(newBook);
+    showCards();
 
-    let container = document.querySelector('#book-list');
-
-    let cardContainer = document.createElement('div');
-    cardContainer.classList.add('card-container');
-
-    let cardId = document.createElement('h1');
-    cardContainer.appendChild(cardId);
-    let cardTitle = document.createElement('h1');
-    cardContainer.appendChild(cardTitle);
-
-    let cardAuthor = document.createElement('h2');
-    cardContainer.appendChild(cardAuthor);
-
-    let cardPages = document.createElement('p');
-    cardContainer.appendChild(cardPages);
-
-    let cardRead = document.createElement('p');
-    cardContainer.appendChild(cardRead);
-
-    container.appendChild(cardContainer);
-
-    cardId.textContent = id;
-    id += 1;
-    cardTitle.textContent = title;
-    cardAuthor.textContent = author;
-    cardPages.textContent = pages;
-    cardRead.textContent = read;
-    cardContainer.appendChild(cardTitle);
-    cardContainer.appendChild(cardAuthor);
-    cardContainer.appendChild(cardPages);
-    cardContainer.appendChild(cardRead);
-    console.log(myLibrary)
+    console.log(myLibrary);
 }
 
+//cuando se aprete el boton deleteButton, busca el id con el dataset y lo elimina del array.
 const openModal = document.querySelector('#open-button');
 
 openModal.addEventListener('click', () => {
     modal.showModal()    
 });
 
+
+
 let container = document.querySelector('#book-list');
 
 
-// LOOP IN EVERY INSTANCE OF THE OBJECT
-// for (const book of myLibrary) {
-//     console.log(book.author); //inserta contenido de cada instancia del objeto
-//     let cardContainer = document.createElement('div');
-//     cardContainer.classList.add('card-container');
+//LOOP IN EVERY INSTANCE OF THE OBJECT
+function showCards() {
+    container.innerHTML = '';
+    for (const book of myLibrary) {
+    
+        let cardContainer = document.createElement('div');
+        cardContainer.classList.add('card-container');
 
-//     let cardTitle = document.createElement('h1');
-//     cardContainer.appendChild(cardTitle);
+        let cardTitle = document.createElement('h1');
+        cardContainer.appendChild(cardTitle);
 
-//     let cardAuthor = document.createElement('h2');
-//     cardContainer.appendChild(cardAuthor);
+        let cardAuthor = document.createElement('h2');
+        cardContainer.appendChild(cardAuthor);
 
-//     let cardPages = document.createElement('p');
-//     cardContainer.appendChild(cardPages);
+        let cardPages = document.createElement('p');
+        cardContainer.appendChild(cardPages);
 
-//     let cardRead = document.createElement('p');
-//     cardContainer.appendChild(cardRead);
+        let cardRead = document.createElement('p');
+        cardContainer.appendChild(cardRead);
 
-//     container.appendChild(cardContainer);
+        container.appendChild(cardContainer);
 
-//     cardTitle.textContent = book.title;
-//     cardAuthor.textContent = book.author;
-//     cardPages.textContent = book.pages;
-//     cardRead.textContent = book.read;
-//     cardContainer.appendChild(cardTitle);
-//     cardContainer.appendChild(cardAuthor);
-//     cardContainer.appendChild(cardPages);
-//     cardContainer.appendChild(cardRead);
-// }
-
-//TODO: Associate every book to an id
-// console.log(myLibrary);
-
-function addBookToLibrary() {
-    //do stuff here
+        cardTitle.textContent = book.title;
+        cardAuthor.textContent = book.author;
+        cardPages.textContent = book.pages;
+        cardRead.textContent = book.read;
+        cardContainer.appendChild(cardTitle);
+        cardContainer.appendChild(cardAuthor);
+        cardContainer.appendChild(cardPages);
+        cardContainer.appendChild(cardRead);
+    }
 }
+//TODO: Associate every book to an id
+console.log(myLibrary);
