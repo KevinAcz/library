f7fb7f9070ee2673c2f74542beccda2eaa45221e
KevinAcz <kevinaceitonsalazar@gmail.com>
2024-02-26 19:36:46 -0300

    ID is linked to every delete button

--- javascript.js
+++ javascript.js
@@ -35,7 +35,9 @@ function submitForm(event) {
     console.log(myLibrary);
 }
 
-//cuando se aprete el boton deleteButton, busca el id con el dataset y lo elimina del array.
+//cuando se aprete el boton deleteButton, segun el dataset que tenga
+//elimina el indice del array en myLibrary y actualiza showCards()
+
 const openModal = document.querySelector('#open-button');
 
 openModal.addEventListener('click', () => {
@@ -43,9 +45,8 @@ openModal.addEventListener('click', () => {
 });
 
 
-
+let i = 0;
 let container = document.querySelector('#book-list');
-
 
 //LOOP IN EVERY INSTANCE OF THE OBJECT
 function showCards() {
@@ -67,17 +68,44 @@ function showCards() {
         let cardRead = document.createElement('p');
         cardContainer.appendChild(cardRead);
 
+        let deleteButton = document.createElement('button');
+        deleteButton.classList.add('delete-button');
+        deleteButton.dataset.id = i;
+
+        cardContainer.appendChild(deleteButton);
+        
+
         container.appendChild(cardContainer);
 
         cardTitle.textContent = book.title;
         cardAuthor.textContent = book.author;
         cardPages.textContent = book.pages;
         cardRead.textContent = book.read;
-        cardContainer.appendChild(cardTitle);
-        cardContainer.appendChild(cardAuthor);
-        cardContainer.appendChild(cardPages);
-        cardContainer.appendChild(cardRead);
+        deleteButton.textContent = 'Delete';
+
+        deleteButton.addEventListener('click', () => {
+            console.log(deleteButton.dataset.id)
+        })
     }
+    i += 1;
 }
+
+// function deleteBook(idToDelete) {
+//     myLibrary.splice(idToDelete, 1);
+// }
+
+// const deleteButtons = document.querySelectorAll('.delete-button');
+
+// function getId() {
+// deleteButtons.forEach(button => {
+
+//     button.addEventListener('click', () => {
+//         console.log(button.dataset.id);
+//     });
+
+// });
+// }
+
+
 //TODO: Associate every book to an id
 console.log(myLibrary);
